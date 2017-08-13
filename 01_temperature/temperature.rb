35939323aef462c61d971d75660366a4ff44d316
Jeremy Nagahama <naga@hawaii.edu>
2017-08-13 11:09:02 -1000

    Finished calc portion

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+def ftoc(temp)
+	num = (temp - 32) / 1.8
+	return num.ceil
+end
+
+def ctof(temp)
+	num = temp * 1.8 + 32
+	return num
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
-#write your code here
+def add(num1, num2)
+	return num1 + num2
+end
+
+def subtract(num1, num2)
+	return num1 - num2
+end
+
+def sum(array)
+	i = 0;
+	sum = 0
+	while i < array.length
+		sum += array[i]
+	end
+	return sum
+end
